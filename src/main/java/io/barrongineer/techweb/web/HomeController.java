c62cd6451efd0fa48002fcc8eb8a4856163272ba
Shaunn Barron <sbarron@netjets.com>
2015-04-30 12:31:59 -0400

    Added spring loaded.

--- src/main/java/io/barrongineer/techweb/web/HomeController.java
+++ src/main/java/io/barrongineer/techweb/web/HomeController.java
@@ -2,15 +2,10 @@ package io.barrongineer.techweb.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.security.Principal;
 
 /**
  * Created by shaunn on 3/29/2015.
@@ -19,9 +14,6 @@ import java.security.Principal;
 public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-    @Autowired
-    private OAuth2RestTemplate oAuth2RestTemplate;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
